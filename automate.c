867a100706234c1cf4d7ffc3c92c30f9249217db
lkilipoutte <laure97.feuillet@orange.fr>
2017-12-13 08:42:18 +0100

    .

--- automate.c
+++ automate.c
@@ -22,11 +22,32 @@ void liberer_automate (struct automate * pautomate)
 	printf("Liberation de l'espace memoire de automate effectue\n");
 }
 
+// Renvoie 0 si l'étiquette n'appartient pas à l'alphabet
+// Renvoie 1 si l'étiquette appartient à l'aplphabet
+int etiquette_existe(struct automate * pautomate, char v)
+{
+	int x = 0;
+	int i = 0;
+	// On regarde si l'étiquette 
+	while((pautomate->alphabet[i] != '/o') && (x == 0))
+	{
+		if (v == pautomate->alphabet[i])
+		{
+			x = 1;
+		}
+		i++;
+	}
+	return x;
+}
+
 // Paramètres : deux états e1 et e2 et une étiquette v
 // Ajoute la transition (e1,v) = e2 
 // Vérifie que e1 et e2 existent, que v appartient à l'alphabet, 
 // qu'il n'y a pas déjà de transition de e1 vers e2
 void ajouter_transition (struct automate * pautomate, int e1, int e2, char v)
 {
+	if ((e1 < pautomate->graphe_trans->nbsommets) && (e2 < pautomate->graphe_trans->nbsommets) && (etiquette_existe(pautomate, v)==1))
+	{
 		ajouter_arc(pautomate->graphe_trans, e1, e2, v);
+	}
 }
