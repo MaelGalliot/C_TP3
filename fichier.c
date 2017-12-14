ccb92da3ff630ae6d4a8839bcbc4d2abbff55091
lkilipoutte <laure97.feuillet@orange.fr>
2017-12-14 17:09:36 +0100

    .

--- fichier.c
+++ fichier.c
@@ -5,6 +5,7 @@
 struct graphe * lire_graphe(FILE * pfichier)
 {
 
+	return pgraphe;
 }
 
 // Paramètres : descripteur de fichier + graphe
@@ -15,3 +16,14 @@ void ecrire_graphe(FILE * pfichier, struct graphe * pgraphe)
 
 // Paramètres : nom de fichier
 // Retourne un AFD alloué en mémoire dans la fonction à partir des données 
+struct automate * lire_automate(char * nomfichier)
+{
+	return pautomate;
+}
+
+// Paramètres : automate + nom de fichier
+// Sauve l'automate dans le fichier
+void ecrire_automate(strucut automate * pautomate, char * nomfichier)
+{
+
+}
