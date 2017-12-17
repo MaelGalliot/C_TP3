830004cc66760e78984452f64a6146b53c2812f4
MaelGalliot <32235552+MaelGalliot@users.noreply.github.com>
2017-12-17 18:38:04 +0100

    Prototype de fonction

--- fichier.h
+++ fichier.h
@@ -1,10 +1,11 @@
-#ifndef FICHIER_H
-#define FICHIER_H
+#ifndef AUTOMATE_H
+#define AUTOMATE_H
 
 #include "graphe.h"
-#include "automate.h"
-#include "langage.h"
 
-
+struct automate * lire_automate(FILE * );
+struct automate * ecrire_automate(FILE *, struct automate *);
+void sauvegarde_automate(struct automate *, char *);
+void lecture(char *, int);
 
 #endif
